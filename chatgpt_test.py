68ef2200821aff944fe5b508131baa57a56650a4
19276YamamotoKeisuke <19276@toba-cmt.ac.jp>
2023-04-22 22:29:10 +0900

    whisper and sounddeivetest

--- chatgpt_test.py
+++ chatgpt_test.py
@@ -1,20 +1,42 @@
 import openai
 import os
-import time
 from datetime import datetime as dt
 from gtts import gTTS
-#from transformers import pipeline
+import sounddevice as sd
+import soundfile as sf
+import numpy as np
 import pygame
+#from transformers import pipeline
 
 # mp3を再生
-def play_mp3(mpfile):
+def play_mp3(output_mpfile):
     pygame.mixer.init(frequency = 44100)
     pygame.mixer.music.set_volume(1.0)
-    print(mpfile)
-    # pygame.mixer.music.load("data/" + mpfile)
-    pygame.mixer.music.load(mpfile)
+    print(output_mpfile)
+    pygame.mixer.music.load(output_mpfile)
     pygame.mixer.music.play(1)
     # time.sleep(15)
+
+# 入力した音源を文字起こし　whisperAPI
+def sound_to_text(input_mpfile):
+    audio_file= open(input_mpfile, "rb")
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)
+    # print(transcript["text"])
+    return transcript["text"]
+
+def record_mp3():
+    fs = 48000
+    duration = 30
+    file = "input_data/user_input.wav"
+    record = sd.rec(duration * fs, samplerate=fs, channels=1)
+    finish_token = input("Speak for the me(If you finish recording, press enter,) :")
+    if finish_token == "":
+        sd.stop
+        sf.write(file, fs, record)
+    elif finish_token == "exit":
+        sd.stop
+        exit()
+    
 
 # token数を減らすためにユーザ入力を英語に、gpt出力を日本語に
 # def translate(text, lang):
@@ -38,12 +60,7 @@ def chatgpt_com(input_t):
     text = res["choices"][0]["message"]["content"]
     #text = translate(text, "en")
     tts = gTTS(text, lang='ja')
-    # ファイルネームが重複しないように時間.mp3で保存
-    #tdatetime = dt.now()
-    #filename = tdatetime.strftime('%d%h%m%s')
-    # filename += ".mp3"
     filename = "data/" + "chatgpt_res.mp3"
-    #filearray.append(filename)
     tts.save(filename)
     return filename
 
@@ -58,26 +75,27 @@ def dummy():
     # pygame.mixer.music.stop()
 
 def main():
-    #global filearray
     # 会話内容を記憶するための配列　未実装
     history = []
-    #filearray = []
     while True:
         inputText = input("Enter your text(If you exit this program, please write exit.):")
         if inputText == "exit":
             pygame.mixer.music.stop()
             break
-        # pygame.mixer.music.stop()
         res_file = chatgpt_com(inputText)
         play_mp3(res_file)
         #dummy()
-        # filearray.append(inputText)
+    
+def setup():
+    openai.api_key = os.getenv("OPENAI_API_KEY")
+    sd.default.device = 2
+
 
 if __name__ == "__main__":
     # apikeyを設定
-    openai.api_key = os.getenv("OPENAI_API_KEY")
     # translator設定 一時凍結
     #en_to_ja_translator = pipeline("translation", model="staka/fugumt-en-ja")
     #ja_to_en_translator = pipeline("translation", model="staka/fugumt-ja-en")
+    setup()
     main()
     # 個人のgoogleアカウントならいけた
